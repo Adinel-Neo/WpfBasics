a366357bb7f288b8bd05fa850c820fc5e4480c99
Adinel de Deus <adineltxt@hotmail.com>
2017-07-12 17:12:57 -0300

    Aula 02 WPF treeview directories

---
+++ WpfTreeView/WpfTreeView/HeaderToImageConverter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace WpfTreeView
+{
+    [ValueConversion(typeof(string), typeof(BitmapImage))]
+    public class HeaderToImageConverter : IValueConverter
+    {
+
+        public static HeaderToImageConverter Instance = new HeaderToImageConverter();
+
+
+        /// <summary>
+        /// Converts full path to a specific image type of a  drive, folder or file
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //get the full path
+            var path = (string)value;
+
+            // if the path is null ignore
+            if (path == null)
+                return null;
+
+            var name = MainWindow.GetFileOrFolderName(path);
+
+            var image = "Imagens/file.png";
+
+            if (string.IsNullOrEmpty(name))
+            {
+                image = "Imagens/drive.png";
+            }
+            else if (new FileInfo(path).Attributes.HasFlag(FileAttributes.Directory))
+            {
+                image = "Imagens/folder-closed.png";
+            }
+
+            return new BitmapImage(new Uri($"pack://application:,,,/{image}"));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
